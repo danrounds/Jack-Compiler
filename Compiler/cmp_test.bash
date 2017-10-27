54c1f3d9ee440ba755ec082cad0191f5350a8ec3
Dan Rounds <dan.rounds@gmail.com>
2017-10-26 21:58:25 -0400

    Update diffs for new test directory structure

--- Compiler/cmp_test.bash
+++ Compiler/cmp_test.bash
@@ -2,16 +2,16 @@
 
 pushd tests/test_code0/Extras_\(Non_Project\)/Maze3d
 
-cmp Cam3D.vm Cam3D_COMPARE.vm
-cmp Display.vm Display_COMPARE.vm
-cmp Float.vm Float_COMPARE.vm
-cmp Grid.vm Grid_COMPARE.vm
-cmp LineBuffer.vm LineBuffer_COMPARE.vm
-cmp M4D.vm M4D_COMPARE.vm
-cmp Main.vm Main_COMPARE.vm
-cmp Point.vm Point_COMPARE.vm
-cmp Tile.vm Tile_COMPARE.vm
-cmp V2D.vm V2D_COMPARE.vm
-cmp V3D.vm V3D_COMPARE.vm
+cmp Cam3D.vm COMPARE/Cam3D_COMPARE.vm
+cmp Display.vm COMPARE/Display_COMPARE.vm
+cmp Float.vm COMPARE/Float_COMPARE.vm
+cmp Grid.vm COMPARE/Grid_COMPARE.vm
+cmp LineBuffer.vm COMPARE/LineBuffer_COMPARE.vm
+cmp M4D.vm COMPARE/M4D_COMPARE.vm
+cmp Main.vm COMPARE/Main_COMPARE.vm
+cmp Point.vm COMPARE/Point_COMPARE.vm
+cmp Tile.vm COMPARE/Tile_COMPARE.vm
+cmp V2D.vm COMPARE/V2D_COMPARE.vm
+cmp V3D.vm COMPARE/V3D_COMPARE.vm
 
 popd
